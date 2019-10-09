bd188a02bcddb8092da8fab74cb12f5c3850c111
Mattia Succurro <matty2000.succ@gmail.com>
2019-10-09 21:08:10 +0200

    Some improvements for repository users

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# coriplus
+# Cori+
+
+A simple social network, inspired by the now dead Google-Plus.
 
 To run the app, run the file "run_example.py"
 
 Based on Tweepee example of [peewee](https://github.com/coleifer/peewee/).
+
+## Features
+
+* Create text statuses, optionally with image
+* Follow users
+* Timeline feed
+* In-site notifications
+* SQLite-based app
--- run_example.py
+++ run_example.py
@@ -3,6 +3,14 @@
 import sys
 sys.path.insert(0, '../..')
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--port', type=int, default=5000,
+    help='An alternative port where to run the server.')
+
 from app import app, create_tables
-create_tables()
-app.run()
+
+if __name__ == '__main__':
+    args = argparse.parse_args()
+    create_tables()
+    app.run(port=args.port)
